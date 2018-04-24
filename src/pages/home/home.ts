30a21c5e3d04a48316330848cb30bd941469be1e
JiaYee <jiayee9797@hotmail.com>
2018-04-24 11:05:50 +0800

    Initial

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,32 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { TouchID } from '@ionic-native/touch-id';
+/**
+ * Generated class for the HomePage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
 
+@IonicPage()
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    private touchId: TouchID,
+    public navCtrl: NavController,
+    public navParams: NavParams
+  ) {
+  }
 
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad HomePage');
+    this.touchId.isAvailable().then((haha) => {
+      alert(haha);    
+    })
   }
 
 }
